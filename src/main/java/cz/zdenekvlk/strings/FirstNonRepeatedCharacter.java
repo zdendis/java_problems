fa01e33a25f97d6f59a5ce99a13ac939bdeec3cf
Zdeněk Vlk <zvlk@csob.cz>
2020-03-03 15:33:58 +0100

    First non repeated character

--- src/main/java/cz/zdenekvlk/strings/FirstNonRepeatedCharacter.java
+++ src/main/java/cz/zdenekvlk/strings/FirstNonRepeatedCharacter.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class FirstNonRepeatedCharacter {
     public static void main(String[] args) {
-        System.out.println(firstNonRepeatedCharacterFunctionalStyle("ahojnask"));
+        System.out.println(firstNonRepeatedCharacterFunctionalStyle("ahobjnasbhok"));
     }
 
     private static String firstNonRepeatedCharacterFunctionalStyle(String str) {
